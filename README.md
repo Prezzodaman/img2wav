aca9f97faff49896309f48c8f47e9da3e0bc8510
Presley Peters <presleypeters04@gmail.com>
2023-08-26 17:33:03 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # img2wav
-Python program that converts an image file to audio
+Python program that converts an image file to audio. The result is a 16-bit wave file, and when viewing its spectrogram, the image can be seen! Images with solid lines work best, but anything will convert, with varying degrees of clarity. Dark backgrounds are recommended, as lighter shades are louder. Indexed images aren't supported right now.
+The only libraries used by this program are Pillow, ArgParse, Wave and Math. No other external libraries are used!
+
+# How's it done?
+This program uses additive synthesis to generate the image, by combining multiple sine waves together. Each vertical pixel has its own frequency, spanning the full frequency range. Then, these frequencies are added together, with the current pixel's intensity deciding the amplitude of each one. This is repeated for every horizontal column of pixels. You can change the "sample size" for each one, giving the sine waves more time to oscillate before going to the next column. This results in a better quality image, but it takes longer to render, and the resulting file will be much longer as well.
---
+++ img2wav.py
@@ -0,0 +1,63 @@
+import argparse
+import wave
+import math
+from PIL import Image
+
+parser=argparse.ArgumentParser(description="Converts an image to an audio file")
+parser.add_argument("input_file", type=argparse.FileType("r"),help="The name of the input image")
+parser.add_argument("output_file", type=argparse.FileType("w"),help="The name of the resulting file")
+parser.add_argument("sample_rate",type=int,help="Sample rate")
+parser.add_argument("sample_size",type=int,help="Sample size (dictates the overall length)")
+parser.add_argument("top_frequency",type=int,help="Top frequency")
+
+args=parser.parse_args()
+
+input_file=args.input_file.name
+output_file=args.output_file.name
+sample_rate=args.sample_rate
+sample_size=args.sample_size
+top_frequency=args.top_frequency
+
+print("Img2wav v1.0.0")
+print("by Presley Peters, 2023")
+print()
+
+file_finished=[]
+
+if top_frequency>sample_rate//2:
+	print("Error: Top frequency must be " + str(sample_rate//2) + " or below!")
+else:
+	with Image.open(input_file) as image:
+		print("Width: " + str(image.width) + ", Height: " + str(image.height))
+		image_x=0
+		angles=[0]*image.height
+		frequency_increase=top_frequency/image.height
+		samples_total=0
+		while image_x<image.width:
+			for sample in range(0,sample_size):
+				frequency=0
+				byte=0
+				print(str(int(samples_total/(sample_size*image.width)*100)) + "% done, X: " + str(image_x+1) + "/" + str(image.width) + ", Sample: " + str(sample+1) + "/" + str(sample_size) + "       ",end="\r")
+				for image_y in range(image.height-1,0,-1):
+					pixel=image.getpixel((image_x,image_y))
+					shade=(pixel[0]+pixel[1]+pixel[2])/3
+					byte+=int((math.sin(angles[image_y])*(shade*255))+32768)//image.height
+					angles[image_y]+=(frequency*math.pi*2)/sample_rate
+					frequency+=frequency_increase
+				samples_total+=1
+				
+				byte=(byte+32768) & 65535
+				file_finished.append(byte & 255)
+				file_finished.append(byte>>8)
+			
+			image_x+=1
+
+		print()
+		print("Done!")
+
+		wave_file=wave.open(output_file,"w")
+		wave_file.setnchannels(1)
+		wave_file.setsampwidth(2)
+		wave_file.setframerate(sample_rate)
+		wave_file.writeframesraw(bytearray(file_finished))
+		wave_file.close()
