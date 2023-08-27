20bfd5180df24941fc6885a5a315a5f0636d3d41
Presley Peters <presleypeters04@gmail.com>
2023-08-27 15:18:52 +0100

    Improved image quality, added a "spacing" option

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # img2wav
-Python program that converts an image file to audio. The result is a 16-bit wave file, and when viewing its spectrogram, the image can be seen! Images with solid lines work best, but anything will convert, with varying degrees of clarity. Dark backgrounds are recommended, as lighter shades are louder. Indexed images aren't supported right now.
+Python program that converts an image file to audio. The result is a 16-bit wave file, and when viewing its spectrogram, the image can be seen! Dark backgrounds are recommended, as lighter shades are louder. Indexed images aren't supported right now.
 The only libraries used by this program are Pillow, ArgParse, Wave and Math. No other external libraries are used!
 
-# How's it done?
+## How's it done?
 This program uses additive synthesis to generate the image, by combining multiple sine waves together. Each vertical pixel has its own frequency, spanning the full frequency range. Then, these frequencies are added together, with the current pixel's intensity deciding the amplitude of each one. This is repeated for every horizontal column of pixels. You can change the "sample size" for each one, giving the sine waves more time to oscillate before going to the next column. This results in a better quality image, but it takes longer to render, and the resulting file will be much longer as well.
+
+When frequencies are close together, it produces a "beating" sound, which can distort the quality of an image. To get around this, a "spacing" option is available, which increases the spacing between frequencies. This is recommended for larger images, because as the image increases in size, more frequencies are added, which are closer together.
--- img2wav.py
+++ img2wav.py
@@ -9,6 +9,7 @@ parser.add_argument("output_file", type=argparse.FileType("w"),help="The name of
 parser.add_argument("sample_rate",type=int,help="Sample rate")
 parser.add_argument("sample_size",type=int,help="Sample size (dictates the overall length)")
 parser.add_argument("top_frequency",type=int,help="Top frequency")
+parser.add_argument("spacing",type=int,help="Spacing between frequencies (smaller images require a lower spacing)")
 
 args=parser.parse_args()
 
@@ -17,8 +18,9 @@ output_file=args.output_file.name
 sample_rate=args.sample_rate
 sample_size=args.sample_size
 top_frequency=args.top_frequency
+spacing=args.spacing
 
-print("Img2wav v1.0.0")
+print("Img2wav v1.0.1")
 print("by Presley Peters, 2023")
 print()
 
@@ -27,30 +29,33 @@ file_finished=[]
 if top_frequency>sample_rate//2:
 	print("Error: Top frequency must be " + str(sample_rate//2) + " or below!")
 else:
+	if spacing<1:
+		print("Warning: Spacing is below 1, using 1...")
+		spacing=1
 	with Image.open(input_file) as image:
-		print("Width: " + str(image.width) + ", Height: " + str(image.height))
+		print("Width: " + str(image.width) + ", Height: " + str(image.height) + ", Top frequency: " + str(top_frequency) + ", Spacing: " + str(spacing))
 		image_x=0
 		angles=[0]*image.height
 		frequency_increase=top_frequency/image.height
 		samples_total=0
-		while image_x<image.width:
-			for sample in range(0,sample_size):
+		sample_size_inc=sample_rate/22050
+		for image_x in range(0,image.width):
+			for sample in range(0,int(sample_size*sample_size_inc)):
 				frequency=0
 				byte=0
-				print(str(int(samples_total/(sample_size*image.width)*100)) + "% done, X: " + str(image_x+1) + "/" + str(image.width) + ", Sample: " + str(sample+1) + "/" + str(sample_size) + "       ",end="\r")
+				print(str(int(samples_total/(sample_size*image.width*sample_size_inc)*100)) + "% done, X: " + str(image_x+1) + "/" + str(image.width) + ", Sample: " + str(int(sample/sample_size_inc)+1) + "/" + str(sample_size) + "       ",end="\r")
 				for image_y in range(image.height-1,0,-1):
 					pixel=image.getpixel((image_x,image_y))
 					shade=(pixel[0]+pixel[1]+pixel[2])/3
-					byte+=int((math.sin(angles[image_y])*(shade*255))+32768)//image.height
-					angles[image_y]+=(frequency*math.pi*2)/sample_rate
+					byte+=int((math.sin(angles[image_y])*shade*255)+32768)//image.height
+					if image_y*sample_size_inc%int(spacing*sample_size_inc)==0:
+						angles[image_y]+=(frequency*math.pi*2)/sample_rate
 					frequency+=frequency_increase
 				samples_total+=1
 				
 				byte=(byte+32768) & 65535
 				file_finished.append(byte & 255)
 				file_finished.append(byte>>8)
-			
-			image_x+=1
 
 		print()
 		print("Done!")
